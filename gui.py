3afd6bea382fa2eae647bb99481d425cdbd76fbf
daylangilliland <daylan.bgill@gmail.com>
2024-05-01 17:47:59 +0300

    Implement the Complete and Exit buttons

--- gui.py
+++ gui.py
@@ -1,14 +1,19 @@
 import functions
-import PySimpleGUI as fsg
+import FreeSimpleGUI as fsg
 
 label = fsg.Text("Type in a to-do:")
 input_box = fsg.InputText(tooltip="Enter a to-do:", key="todo")
 add_button = fsg.Button("Add")
 list_box = fsg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=(45, 10))
 edit_button = fsg.Button("Edit")
+complete_button = fsg.Button("Complete")
+exit_button = fsg.Button("Exit")
 
 window = fsg.Window('My To-Do App',
-                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                    layout=[[label],
+                            [input_box, add_button],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
                     font=('Helvetica', 20))
 
 while True:
@@ -33,6 +38,17 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
+        case 'Exit':
+            break
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
